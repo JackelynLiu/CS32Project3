f2c9329ae7c5fd57f9b8d9eb1056ef4abb849fb2
Jackelyn Liu <jackelyn.d.liu@gmail.com>
2019-02-18 16:29:37 -0800

    Update Actor.cpp

--- Actor.cpp
+++ Actor.cpp
@@ -4,12 +4,14 @@
 #include "StudentWorld.h"
 
 Actor::Actor(int imageID, double x, double y, int dir, int depth)
-	:GraphObject(imageID, x, y, dir, depth) {}
-
-Penelope::Penelope(double x, double y)
-	:Actor(IID_PLAYER, x, y, 0, 0)
+	:GraphObject(imageID, x, y, dir, depth)
 {
+}
 
+Penelope::Penelope(double x, double y, StudentWorld* sw)
+	: Actor(IID_PLAYER, x, y, right, 0)
+{
+	studw = sw;
 }
 
 Penelope::~Penelope()
@@ -17,11 +19,42 @@ Penelope::~Penelope()
 	delete this;
 }
 
-void Penelope::doSomething() {}
+void Penelope::doSomething()
+{
+	if (studw->getLives() == 0)
+		return;
+	int ch;
+	if (studw->getKey(ch))
+	{
+		switch (ch)
+		{
+		case KEY_PRESS_LEFT:
+			setDirection(left);
+			moveTo(getX() - 4, getY());
+			break;
+		case KEY_PRESS_RIGHT:
+			setDirection(right);
+			moveTo(getX() + 4, getY());
+			break;
+		case KEY_PRESS_DOWN:
+			setDirection(down);
+			moveTo(getX(), getY() - 4);
+			break;
+		case KEY_PRESS_UP:
+			setDirection(up);
+			moveTo(getX(), getY() + 4);
+			break;
+		/*case KEY_PRESS_SPACE:
+			break*/
+		default:
+			return;
+		}
+	}
+}
 
 
 Wall::Wall(double x, double y)
-	:Actor(IID_WALL,x, y, 0, 0)
+	:Actor(IID_WALL,x, y, right, 0)
 {
 
 }
