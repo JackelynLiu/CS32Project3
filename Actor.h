55d130390dbf5349d0cc11a09c3d6841b510a96c
Jackelyn Liu <jackelyn.d.liu@gmail.com>
2019-02-24 22:10:31 -0800

    Update Actor.h

--- Actor.h
+++ Actor.h
@@ -11,6 +11,8 @@ public:
 	//virtual ~Actor();
 	virtual void doSomething() = 0;
 	virtual bool blocksMovement() const = 0;
+	virtual bool canbeDamaged() const;
+	virtual bool canbeInfected() const;
 	bool getStatus() const;
 	void setStatus(bool new_status);
 	int gettickcount() const;
@@ -30,6 +32,7 @@ public:
 	MovingObjects(StudentWorld* sw, int imageID, double x, double y);
 	virtual void doSomething() = 0;
 	virtual bool blocksMovement() const;
+	virtual bool canbeDamaged() const;
 };
 
 class Person :public MovingObjects
@@ -37,6 +40,7 @@ class Person :public MovingObjects
 public:
 	Person(StudentWorld* sw, int imageID, double x, double y);
 	virtual void doSomething() = 0;
+	virtual bool canbeInfected() const;
 	bool getInfectedStatus() const;
 	void setInfectedStatus(bool infected);
 	int getInfectionCount() const;
@@ -88,14 +92,6 @@ public:
 	virtual void doSomething();
 };
 
-class StillObjects :public Actor
-{
-public:
-	StillObjects(StudentWorld* sw, int imageID, double x, double y, int dir, int depth);
-	virtual void doSomething() = 0;
-	virtual bool blocksMovement() const;
-};
-
 class Wall :public Actor
 {
 public:
@@ -103,6 +99,14 @@ public:
 	virtual void doSomething();
 	virtual bool blocksMovement() const;
 	virtual std::string defineObjectType() const;
+};
+
+class StillObjects :public Actor
+{
+public:
+	StillObjects(StudentWorld* sw, int imageID, double x, double y, int dir, int depth);
+	virtual void doSomething() = 0;
+	virtual bool blocksMovement() const;
 };
 
 class Exit :public StillObjects
@@ -127,6 +131,7 @@ public:
 	Goodie(StudentWorld* sw, int imageID, double x, double y);
 	virtual void doSomething() = 0;
 	virtual std::string defineObjectType() const;
+	virtual bool canbeDamaged() const;
 };
 
 class VaccineGoodie :public Goodie
@@ -156,6 +161,7 @@ public:
 	Landmine(StudentWorld* sw, double x, double y);
 	virtual void doSomething();
 	virtual std::string defineObjectType() const;
+	virtual bool canbeDamaged() const;
 };
 
 class Projectile :public StillObjects
