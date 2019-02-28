3f771c909a11f5178b3fd9d4008e33194ce1122e
Jackelyn Liu <jackelyn.d.liu@gmail.com>
2019-02-27 22:50:40 -0800

    Update Actor.cpp

--- Actor.cpp
+++ Actor.cpp
@@ -2,14 +2,16 @@
 
 #include "Actor.h"
 #include "StudentWorld.h"
+#include <map>
+using namespace std;
 
 Actor::Actor(StudentWorld* sw, int imageID, double x, double y, int dir, int depth)
-	:GraphObject(imageID, x, y, dir, depth), current_world(sw), m_status(true), m_tickcount(0), m_infectedstatus(false)
+	:GraphObject(imageID, x, y, dir, depth), current_world(sw), m_status(true), m_tickcount(0)
 {}
 
 bool Actor::getStatus() const { return m_status; }
 
-void Actor::setStatus(bool new_status) { m_status = new_status; }
+void Actor::changeStatus() { m_status = false; }
 
 int Actor::gettickcount() const { return m_tickcount; }
 
@@ -20,10 +22,6 @@ bool Actor::canbeDamaged() const { return false; }
 bool Actor::canbeInfected() const { return false; }
 
 bool Actor::blocksFlame() const { return false; }
-
-bool Actor::getInfectedStatus() const { return m_infectedstatus; }
-
-void Actor::setInfectedStatus(bool infected) { m_infectedstatus = infected; }
 
 bool Actor::canExit() const { return false; }
 
@@ -49,18 +47,16 @@ bool MovingObjects::canbeDamaged() const { return true; }
 bool MovingObjects::canactivateLandmine() const { return true; }
 
 Person::Person(StudentWorld* sw, int imageID, double x, double y)
-	:MovingObjects(sw, imageID, x, y), m_infectioncount(0)
+	:MovingObjects(sw, imageID, x, y), m_infectioncount(0), m_infectedstatus(false)
 {}
 
 void Person::doSomething()
 {
 	if (!getStatus())
 		return;
-	if (getInfectedStatus())
+	if (m_infectedstatus)
 		m_infectioncount++;
 }
-
-std::string Person::defineObjectType() const { return "PERSON"; }
 
 bool Person::canbeInfected() const { return true; }
 
@@ -69,6 +65,10 @@ int Person::getInfectionCount() const { return m_infectioncount; }
 void Person::setInfectionCount(int n) { m_infectioncount = n; }
 
 bool Person::canExit() const { return true; }
+
+bool Person::getInfectedStatus() const { return m_infectedstatus; }
+
+void Person::setInfectedStatus(bool infected) { m_infectedstatus = infected; }
 
 Penelope::Penelope(StudentWorld* sw, double x, double y)
 	:Person(sw, IID_PLAYER, x, y), num_vaccines(0), num_landmines(0), num_flamecharges(0)
@@ -79,7 +79,7 @@ void Penelope::doSomething()
 	Person::doSomething();
 	if (getInfectionCount() == 500)
 	{
-		setStatus(false);
+		changeStatus();
 		getWorld()->playSound(SOUND_PLAYER_DIE);
 		return;
 	}
@@ -215,6 +215,12 @@ Citizen::Citizen(StudentWorld* sw, double x, double y)
 	:Person(sw, IID_CITIZEN, x, y)
 {}
 
+void Citizen::changeStatus() 
+{
+	Actor::changeStatus();
+	getWorld()->increaseScore(-1000);
+}
+
 void Citizen::doSomething()
 {
 	increasetickcount();
@@ -223,7 +229,7 @@ void Citizen::doSomething()
 	double current_y = getY();
 	if (getInfectionCount() == 500)
 	{
-		setStatus(false);
+		changeStatus();
 		getWorld()->playSound(SOUND_ZOMBIE_BORN);
 		getWorld()->increaseScore(-1000);
 		Actor* new_Zombie;
@@ -236,10 +242,14 @@ void Citizen::doSomething()
 	}
 	if (gettickcount() % 2 == 0)
 		return;
-	
+
+	double dist_p = getWorld()->distanceFromPenelope(current_x, current_y);
+	double dist_z = getWorld()->distanceFromNearestZombie(current_x, current_y);
+
 	double px = getWorld()->getPenelopexcoord();
 	double py = getWorld()->getPenelopeycoord();
-	if (getWorld()->whattofollow(current_x, current_y) == 1)
+
+	if (dist_p <= dist_z && dist_p <= 80)
 	{
 		if (current_y == py)			//if Penelope is in the same row
 		{
@@ -374,31 +384,227 @@ void Citizen::doSomething()
 			}
 		}
 	}
-	if (getWorld()->whattofollow(current_x, current_y) == 2)
+	else if (dist_z <= 80)
 	{
-		return;
+		map<double, Direction> dist2dir;
+		double distR, distL, distU, distD;
+		double dist_max = dist_z;
+		Direction dir_max = right;
+
+		if (!getWorld()->containsObstacle(current_x, current_y, current_x + 2, current_y))
+		{
+			distR = getWorld()->distanceFromNearestZombie(current_x + 2, current_y);
+			dist2dir[distR] = right;
+		}
+		if (!getWorld()->containsObstacle(current_x, current_y, current_x - 2, current_y))
+		{
+			distL = getWorld()->distanceFromNearestZombie(current_x - 2, current_y);
+			dist2dir[distL] = left;
+		}
+		if (!getWorld()->containsObstacle(current_x, current_y, current_x, current_y + 2))
+		{
+			distU = getWorld()->distanceFromNearestZombie(current_x, current_y + 2);
+			dist2dir[distU] = up;
+		}
+		if (!getWorld()->containsObstacle(current_x, current_y, current_x, current_y - 2))
+		{
+			distD = getWorld()->distanceFromNearestZombie(current_x, current_y - 2);
+			dist2dir[distD] = down;
+		}
+
+		//map is in ascending order so last one is max
+		map<double, Direction>::iterator it;
+		it = dist2dir.end();
+		it--;
+
+		dist_max = it->first;
+		if (dist_max >= dist_z)
+			dir_max = it->second;
+		else return;
+
+		setDirection(dir_max);
+		switch (dir_max)
+		{
+		case right:
+		{
+			moveTo(current_x + 2, current_y);
+			break;
+		}
+		case left:
+		{
+			moveTo(current_x - 2, current_y);
+			break;
+		}
+		case up:
+		{
+			moveTo(current_x, current_y + 2);
+			break;
+		}
+		case down:
+		{
+			moveTo(current_x, current_y - 2);
+			break;
+		}
+
+		}
 	}
+	else return;
 }
 
 Zombie::Zombie(StudentWorld* sw, double x, double y)
-	:MovingObjects(sw, IID_ZOMBIE, x, y)
+	:MovingObjects(sw, IID_ZOMBIE, x, y), movement_plan(0)
 {}
 
-std::string Zombie::defineObjectType() const { return "ZOMBIE"; }
+void Zombie::doSomething()
+{
+	increasetickcount();
+	if (!getStatus()) return;
+	if (gettickcount() % 2 == 0) return;
+	double vomit_x = 0;
+	double vomit_y = 0;
+	int r = randInt(1, 3);
+	switch (getDirection())
+	{
+	case right:
+	{
+		vomit_x = getX() + SPRITE_WIDTH;
+		vomit_y = getY();
+		if (getWorld()->isZombieVomitTriggerAt(vomit_x, vomit_y))
+		{
+			if (r == 1)
+			{
+				getWorld()->addintovector(new Vomit(getWorld(), vomit_x, vomit_y, right));
+				getWorld()->playSound(SOUND_ZOMBIE_VOMIT);
+			}
+		}
+		break;
+	}
+	case left:
+	{
+		vomit_x = getX() - SPRITE_WIDTH;
+		vomit_y = getY();
+		if (getWorld()->isZombieVomitTriggerAt(vomit_x, vomit_y))
+		{
+			if (r == 1)
+			{
+				getWorld()->addintovector(new Vomit(getWorld(), vomit_x, vomit_y, left));
+				getWorld()->playSound(SOUND_ZOMBIE_VOMIT);
+			}
+		}
+		break;
+	}
+	case up:
+	{
+		vomit_x = getX();
+		vomit_y = getY() + SPRITE_HEIGHT;
+		if (getWorld()->isZombieVomitTriggerAt(vomit_x, vomit_y))
+		{
+			if (r == 1)
+			{
+				getWorld()->addintovector(new Vomit(getWorld(), vomit_x, vomit_y, up));
+				getWorld()->playSound(SOUND_ZOMBIE_VOMIT);
+			}
+		}
+		break;
+	}
+	case down:
+	{
+		vomit_y = getX();
+		vomit_y = getY() - SPRITE_HEIGHT;
+		if (getWorld()->isZombieVomitTriggerAt(vomit_x, vomit_y))
+		{
+			if (r == 1)
+			{
+				getWorld()->addintovector(new Vomit(getWorld(), vomit_x, vomit_y, down));
+				getWorld()->playSound(SOUND_ZOMBIE_VOMIT);
+			}
+		}
+		break;
+	}
+	}
+}
+
+int Zombie::getmovementPlan() const { return movement_plan; }
+void Zombie::setmovementPlan(int num) { movement_plan = num; }
 
 SmartZombie::SmartZombie(StudentWorld* sw, double x, double y)
 	:Zombie(sw, x, y)
 {}
 
+void SmartZombie::changeStatus()
+{
+	Actor::changeStatus();
+	getWorld()->increaseScore(2000);
+}
+
 void SmartZombie::doSomething()
-{}
+{
+	Zombie::doSomething();
+
+
+}
 
 DumbZombie::DumbZombie(StudentWorld* sw, double x, double y)
 	:Zombie(sw, x, y)
 {}
 
-void DumbZombie::doSomething()
-{}
+void DumbZombie::changeStatus()
+{
+	Actor::changeStatus();
+	getWorld()->increaseScore(1000);
+}
+
+void DumbZombie::doSomething()	//carrying vaccine -- remember to implement
+{
+	Zombie::doSomething();
+	
+	if (getmovementPlan() == 0)
+	{
+		int r = randInt(3, 10);
+		setmovementPlan(r);
+		int d = randInt(1, 4);
+		switch (d)
+		{
+		case 1:
+			setDirection(right);
+			break;
+		case 2:
+			setDirection(left);
+			break;
+		case 3:
+			setDirection(down);
+			break;
+		case 4:
+			setDirection(up);
+			break;
+		}
+		setmovementPlan(r);
+	}
+
+	double dest_x = getX();
+	double dest_y = getY();
+	switch (getDirection())
+	{
+	case right:
+		dest_x++;
+		break;
+	case left:
+		dest_x--;
+		break;
+	case up:
+		dest_y++;
+		break;
+	case down:
+		dest_y--;
+		break;
+	}
+	if (!getWorld()->containsObstacle(getX(), getY(), dest_x, dest_y) && !getWorld()->containsPlayer(dest_x, dest_y))
+	{
+		moveTo(dest_x, dest_y);
+		setmovementPlan(getmovementPlan() - 1);
+	}
+	else setmovementPlan(0);
+}
 
 Wall::Wall(StudentWorld* sw, double x, double y)
 	:Actor(sw, IID_WALL,x, y, right, 0)
@@ -407,8 +613,6 @@ Wall::Wall(StudentWorld* sw, double x, double y)
 void Wall::doSomething() {}
 
 bool Wall::blocksMovement() const { return true; }
-
-std::string Wall::defineObjectType() const { return "WALL"; }
 
 bool Wall::blocksFlame() const { return true; }
 
@@ -436,8 +640,6 @@ void Exit::doSomething()
 	}
 }
 
-std::string Exit::defineObjectType() const { return "EXIT"; }
-
 bool Exit::blocksFlame() const { return true; }
 
 Pit::Pit(StudentWorld* sw, double x, double y)
@@ -453,8 +655,6 @@ void Pit::doSomething()
 	getWorld()->getKilledbyFlameorPit(getX(), getY());
 }
 
-std::string Pit::defineObjectType() const { return "PIT"; }
-
 Goodie::Goodie(StudentWorld* sw, int imageID, double x, double y)
 	:StillObjects(sw, imageID, x, y, right, 1)
 {}
@@ -464,15 +664,13 @@ void Goodie::doSomething()
 	if (!getStatus()) return;
 	if (getWorld()->determineOverlapwithPlayer(getX(), getY()))
 	{
-		setStatus(false);
+		changeStatus();
 		getWorld()->playSound(SOUND_GOT_GOODIE);
 		getWorld()->increaseScore(50);
 		getWorld()->pickupGoodies(getX(), getY());
 		
 	}
 }
-
-std::string Goodie::defineObjectType() const { return "GOODIE"; }
 
 bool Goodie::canbeDamaged() const { return true; }
 
@@ -539,7 +737,7 @@ void Landmine::doSomething()
 
 void Landmine::explode()
 {
-	setStatus(false);
+	changeStatus();
 	getWorld()->playSound(SOUND_LANDMINE_EXPLODE);
 	getWorld()->addintovector(new Flame(getWorld(), getX(), getY(), right));
 	getWorld()->addintovector(new Flame(getWorld(), getX() - SPRITE_WIDTH, getY(), right));
@@ -552,8 +750,6 @@ void Landmine::explode()
 	getWorld()->addintovector(new Flame(getWorld(), getX() + SPRITE_WIDTH, getY() - SPRITE_HEIGHT, right));
 	getWorld()->addintovector(new Pit(getWorld(), getX(), getY()));
 }
-
-std::string Landmine::defineObjectType() const { return "LANDMINE"; }
 
 Projectile::Projectile(StudentWorld* sw, int imageID, double x, double y, int dir)
 	:StillObjects(sw, imageID, x, y, dir, 0)
@@ -569,7 +765,7 @@ void Flame::doSomething()
 	if (!getStatus()) return;
 	else if (gettickcount() == 2)
 	{
-			setStatus(false);
+			changeStatus();
 			return;
 	}
 	else
@@ -577,8 +773,6 @@ void Flame::doSomething()
 		getWorld()->getKilledbyFlameorPit(getX(), getY());
 	}
 }
-
-std::string Flame::defineObjectType() const { return "FLAME"; }
 
 bool Flame::canactivateLandmine() const { return true; }
 
@@ -588,8 +782,6 @@ Vomit::Vomit(StudentWorld* sw, double x, double y, int dir)
 	getWorld()->playSound(SOUND_ZOMBIE_VOMIT);
 }
 
-std::string Vomit::defineObjectType() const { return "VOMIT"; }
-
 void Vomit::doSomething()
 {
 	increasetickcount();
@@ -597,7 +789,7 @@ void Vomit::doSomething()
 		return;
 	else if (gettickcount() == 2)
 	{
-		setStatus(false);
+		changeStatus();
 		return;
 	}
 	else
